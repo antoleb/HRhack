f4fd4f74dab2b047e6ba438fadad2077f27b6d19
ksemianov <semyanovk@gmail.com>
2017-11-10 14:48:38 +0300

    Fixed ids_by_position, implemented skills_by_position functions from class DataHandler

--- DataHandler.py
+++ DataHandler.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-class DataHandler:
+class DataHandler():
     def __init__(self, skills, courses, communication, movement, performance):
         self.skills = skills
         self.courses = courses
@@ -10,15 +10,14 @@ class DataHandler:
         self.movement = movement
         self.performance = performance
 
-        self.movement['full_position'] = self.movement[['position', 'Department']].apply(
-            lambda x: "{}/{}".format(x[0], x[1]),
-            axis=1)
+        self.movement['full_position'] = self.movement[['position', 'Department']].apply(lambda x: "{}/{}".format(x[0], x[1]),
+    axis=1)
 
     def skills_by_id(self, id):
         """
         returns: list of srtings from self.skills
         """
-        return self.skills[self.skills.ID == id].TAGNAME.values
+        return self.skills[skills.ID == id].TAGNAME.values
 
     def positions_by_id(self, id):
         """
@@ -30,10 +29,22 @@ class DataHandler:
         """
         returns: ids
         """
-        return list( movement.loc[lambda df: df['full_position']==full_position, :]['id'] )
+        return self.movement.loc[lambda df: df['full_position']==full_position, :]['id'].values
 
     def skills_by_position(self, position):
         """
         returns: [id, performance, skills]
         """
-        pass
+        ids = self.ids_by_position(position)
+        
+        pe = performance.loc[lambda df: df['ID'].isin(ids), :].set_index('ID')
+        sk = skills.loc[lambda df: df['ID'].isin(ids), :].groupby('ID').agg(lambda x: x.tolist())
+        df = pd.concat([pe,sk], axis=1)
+        
+        perf_col =['2017 I Полугодие', '2016 II Полугодие', '2016 I Полугодие',
+       '2015 II Полугодие', '2015 I Полугодие', '2014 II Полугодие',
+       '2014 I Полугодие']
+        df['performance'] = df[perf_col].values.tolist()
+        
+        need_col =['performance', 'TAGNAME']
+        return df1[need_col].reset_index().values
