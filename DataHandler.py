bfe4a477047b2d9c66d9e01cd20ff900d85fa92c
antoleb <anleb96@gmail.com>
2017-11-10 13:29:25 +0300

    DataHandler and NaiveBias

---
+++ DataHandler.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+
+
+class DataHandler:
+    def __init__(self, skills, courses, communication, movement, performance):
+        self.skills = skills
+        self.courses = courses
+        self.communication = communication
+        self.movement = movement
+        self.performance = performance
+
+        self.movement['full_position'] = self.movement[['position', 'Department']].apply(
+            lambda x: "{}/{}".format(x[0], x[1]),
+            axis=1)
+
+    def skills_by_id(self, id):
+        """
+        returns: list of srtings from self.skills
+        """
+        return self.skills[self.skills.ID == id].TAGNAME.values
+
+    def positions_by_id(self, id):
+        """
+        returns: string list from self.movement
+        """
+        return self.movement[self.movement.id == id]['full_position'].values
+
+    def ids_by_position(self, position):
+        """
+        returns: ids
+        """
+        pass
+
+    def skills_by_position(self, position):
+        """
+        returns: [id, performance, skills]
+        """
+        pass
