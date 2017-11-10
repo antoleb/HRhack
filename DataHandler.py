8dd31019487d95d3ec494e039f6dbecf923703d0
antoleb <anleb96@gmail.com>
2017-11-10 15:08:00 +0300

    My version of Kostya fix

--- DataHandler.py
+++ DataHandler.py
@@ -44,8 +44,8 @@ class DataHandler:
         """
         ids = self.ids_by_position(position)
 
-        pe = performance.loc[lambda df: df['ID'].isin(ids), :].set_index('ID')
-        sk = skills.loc[lambda df: df['ID'].isin(ids), :].groupby('ID').agg(lambda x: x.tolist())
+        pe = self.performance.loc[lambda df: df['ID'].isin(ids), :].set_index('ID')
+        sk = self.skills.loc[lambda df: df['ID'].isin(ids), :].groupby('ID').agg(lambda x: x.tolist())
         df = pd.concat([pe, sk], axis=1)
 
         perf_col =['2017 I Полугодие', '2016 II Полугодие', '2016 I Полугодие',
@@ -54,7 +54,7 @@ class DataHandler:
         df['performance'] = df[perf_col].values.tolist()
 
         need_col =['performance', 'TAGNAME']
-        return df1[need_col].reset_index().values
+        return df[need_col].reset_index().values
 
     def remove_id(self, id):
         """
